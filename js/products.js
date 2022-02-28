c2c4a5721d932fbf0c9053e141c3457a0daa9a77
zenabii <maria.turtles@outlook.com>
2022-03-01 00:43:00 +0100

    icons

--- js/products.js
+++ js/products.js
@@ -16,11 +16,16 @@ const productsUrl = baseUrl + "products";
             container.innerHTML += `<div class="product-container">
                                         <a href="product.html?id=${product.id}">
                                         <img src="${product.image.formats.medium.url}" class="product-img" alt="${product.image.alternativeText}">
-                                        <div class="product-info">
-                                            <a href="product.html?id=${product.id}" class="product-title">${product.title}</a>
-                                            <a href="product.html?id=${product.id}" class="product-title">${product.price} NOK</a>
-                                        </div>
                                         </a>
+                                        <div class="product-info-container">
+                                            <div class="product-info">
+                                                <a href="product.html?id=${product.id}" class="product-title">${product.title}</a>
+                                                <a href="product.html?id=${product.id}" class="product-title">${product.price} NOK</a>
+                                            </div>
+                                            <div class="product-info">
+                                            <i class="fa-regular fa-heart fa-2x"></i>
+                                            </div>
+                                        </div>              
                                     </div>
             
             `
