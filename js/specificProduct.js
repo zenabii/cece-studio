1a148694925d6ad50add956e60f468e2dce1f374
zenabii <maria.turtles@outlook.com>
2022-03-02 22:59:41 +0100

    localstorage

--- js/specificProduct.js
+++ js/specificProduct.js
@@ -20,24 +20,61 @@ async function getProduct(productUrl) {
     productName.innerHTML= product.title;
     productPrice.innerHTML = product.price + "NOK";
     productDescription.innerHTML = product.description;
-    addToCart.innerHTML += `<a href="#" class="shopButtonBlack">add to cart <i class="fa-solid fa-heart fa-2x"></i></a>`;
+    addToCart.innerHTML = `<a href="#" class="shopButtonBlack">add to cart <i class="fa-solid fa-heart fa-2x" data-title="${product.title}" data-price="${product.price}" data-img="${product.image.formats.large.url}"></i></a>`;
 
 
-}
+    addProductToCart();
+};
 
 getProduct(productUrl);
 
 
-const cartButton = document.querySelectorAll(".button-container i");
+function addProductToCart() {
+    const cartButton = document.querySelectorAll(".button-container i");
 
-console.log(cartButton)
-
-cartButton.forEach((button) => {
-    button.addEventListener("click", handleClick);
-});
-
+    cartButton.forEach((button) => {
+        button.addEventListener("click", handleClick);
+    });
+};
 
 function handleClick() {
-    console.log(event);
+    this.classList.toggle("fa");
+    this.classList.toggle("far");
 
+    const title = this.dataset.title;
+    const price = this.dataset.price;
+    const img = this.dataset.img;
+
+    const currentFavs = getExistingProducts();
+
+    const productExists = currentFavs.find(function(fav) {
+        return fav.title === title;
+    });
+
+    if(productExists === undefined) {
+        const favProduct = { title: title, price: price, img: img};
+        currentFavs.push(favProduct);
+        saveFavs(currentFavs)
+    }
+    else {
+        const newFavs = currentFavs.filter(fav => fav.title !== title);
+        saveFavs(newFavs);
+    }
+
+};
+
+function getExistingProducts() {
+
+    const favs = localStorage.getItem("favorites");
+
+    if(!favs) {
+        return [];
+    }
+    else{
+        return JSON.parse(favs);
+    }
+}
+
+function saveFavs(favs) {
+    localStorage.setItem("favorites", JSON.stringify(favs));
 }
